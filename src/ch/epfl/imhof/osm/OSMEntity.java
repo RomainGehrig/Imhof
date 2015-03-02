614842156f17a7cc59232054ca7d574aea39ba22
yuratak <yura.tak@epfl.ch>
2015-03-02 14:11:47 +0100

    Etape4(2.1&2.2)

---
+++ src/ch/epfl/imhof/osm/OSMEntity.java
@@ -0,0 +1,99 @@
+package ch.epfl.imhof.osm;
+
+import java.util.Collections;
+
+import ch.epfl.imhof.Attributes;
+
+/**
+ * sert de classe mère à toutes les classes représentant les entités OSM
+ * @author Yura Tak (247528)
+ * @author Romain Gehrig (223316)
+ *
+ */
+public abstract class OSMEntity {
+    private long id;
+    private Attributes attributes;
+    
+    /**
+     * construit une entité OSM dotée de l'identifiant unique et des attributs donnés
+     * @param id L'identifiant unique
+     * @param attributes Attributs donnés
+     */
+    public OSMEntity(long id, Attributes attributes){
+        this.id = id;
+        this.attributes = attributes;
+    }
+    
+    /**
+     * retourne l'identifiant unique de l'entité
+     * @return l'identifiant unique
+     */
+    public long id(){
+        return id;
+    }
+    
+    /**
+     * retourne vrai si et seulement si l'entité possède l'attribut passé en argument.
+     * @param key La clef passée en argument
+     * @return vrai ssi l'entité possède le key
+     */
+    public boolean hasAttribute(String key){
+        return attributes.contains(key);
+    }
+    
+    /**
+     * retourne la valeur de l'attribut donné
+     * @param key La clef passée en argument
+     * @return la valeur de l'attribut donné ou null si celui-ci n'existe pas
+     */
+    public String attributeValue(String key){
+        return attributes.get(key);
+    }
+    
+    /**
+     * sert de classe mère à toutes les classes de bâtisseurs d'entités OSM
+     * @author Yura Tak (247528)
+     * @author Romain Gehrig (223316)
+     *
+     */
+    public final static class Builder{
+        
+        private long id;
+        private Attributes.Builder attributes = new Attributes.Builder();
+        private boolean isIncomplete = false;
+        
+        /**
+         * construit un bâtisseur pour une entité OSM identifiée par l'entier donné
+         * @param id L'identifiant unique
+         */
+        public Builder(long id){
+            this.id = id;
+        }
+        
+        /**
+         * ajoute l'association (clef, valeur) donnée à l'ensemble d'attributs de l'entité en cours de construction. 
+         * Si un attribut de même nom avait déjà été ajouté précédemment, sa valeur est remplacée par celle donnée
+         * @param key La clef
+         * @param value La valeur
+         */
+        public void setAttribute(String key, String value){
+            attributes.put(key, value);
+        }
+        
+        /**
+         * déclare que l'entité en cours de construction est incomplète
+         */
+        public void setIncomplete(){
+            isIncomplete = true;
+        }
+        
+        /**
+         * retourne vrai si et seulement si l'entité en cours de construction est incomplète, 
+         * c-à-d si la méthode setIncomplete a été appelée au moins une fois sur ce bâtisseur depuis sa création.
+         * @return
+         */
+        public boolean isIncomplete(){
+            return isIncomplete;
+        }
+    }
+}
