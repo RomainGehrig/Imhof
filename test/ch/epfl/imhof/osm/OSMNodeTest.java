78b907d7167ad0f3ea37aaf4bedb7fb8cec2a9d3
Romain Gehrig <romain.gehrig@gmail.com>
2015-03-13 10:49:38 +0100

    Confict merged

--- test/ch/epfl/imhof/osm/OSMNodeTest.java
+++ test/ch/epfl/imhof/osm/OSMNodeTest.java
@@ -52,47 +52,21 @@ public class OSMNodeTest {
         OSMNode.Builder b = new OSMNode.Builder(10, this.pt);
         b.build();
     }
-=======
-import org.junit.Test;
-
-import ch.epfl.imhof.PointGeo;
-
-public class OSMNodeTest {
     
-    private PointGeo p = new PointGeo(0, 0);
-    private OSMNode n;
-
     @Test(expected = NullPointerException.class)
     public void constructorOSMNodeTest() {
-        n = new OSMNode (0, p, null);
+        n = new OSMNode (0, pt, null);
     }
     
     @Test
     public void  BuilderTest(){
-        OSMNode.Builder b = new OSMNode.Builder(384723942929L, p);
+        OSMNode.Builder b = new OSMNode.Builder(384723942929L, pt);
     }
     
     @Test
     public void BuilderNegatifTest(){
-        OSMNode.Builder bNeg = new OSMNode.Builder(-1234, p);
+        OSMNode.Builder bNeg = new OSMNode.Builder(-1234, pt);
     }
     
-    @Test(expected = NullPointerException.class)
-    public void BuilderNullTest(){
-        OSMNode.Builder bNull = new OSMNode.Builder(0, null);
-    }
-    
-    @Test(expected = IllegalStateException.class)
-    public void buildIncompletTest(){
-        OSMNode.Builder b0 = new OSMNode.Builder(0, p);
-        b0.setIncomplete();
-        b0.build();
-    }
-    
-    @Test
-    public void buildCompletTest(){
-        OSMNode.Builder b1 = new OSMNode.Builder(0, p);
-        b1.build();
-    }
-
 }
+
