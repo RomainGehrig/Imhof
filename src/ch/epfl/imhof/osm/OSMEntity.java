121c30dec977d97d4b304970392c721c2915c4a7
Romain Gehrig <romain.gehrig@gmail.com>
2015-03-13 10:35:14 +0100

    Some OSM tests

--- src/ch/epfl/imhof/osm/OSMEntity.java
+++ src/ch/epfl/imhof/osm/OSMEntity.java
@@ -9,19 +9,22 @@ import ch.epfl.imhof.Attributes;
  *
  */
 public abstract class OSMEntity {
-    private long id;
+    protected long id;
     private Attributes attributes;
-    
+
     /**
      * construit une entité OSM dotée de l'identifiant unique et des attributs donnés
      * @param id L'identifiant unique
      * @param attributes Attributs donnés
      */
     public OSMEntity(long id, Attributes attributes){
+        if (attributes == null)
+            throw new NullPointerException("Attributes cannot be null.");
+
         this.id = id;
         this.attributes = attributes;
     }
-    
+
     /**
      * retourne l'identifiant unique de l'entité
      * @return l'identifiant unique
@@ -29,7 +32,7 @@ public abstract class OSMEntity {
     public long id(){
         return id;
     }
-    
+
     /**
      * retourne vrai si et seulement si l'entité possède l'attribut passé en argument.
      * @param key La clef passée en argument
@@ -38,7 +41,7 @@ public abstract class OSMEntity {
     public boolean hasAttribute(String key){
         return attributes.contains(key);
     }
-    
+
     /**
      * retourne la valeur de l'attribut donné
      * @param key La clef passée en argument
@@ -47,7 +50,7 @@ public abstract class OSMEntity {
     public String attributeValue(String key){
         return attributes.get(key);
     }
-    
+
     /**
      * sert de classe mère à toutes les classes de bâtisseurs d'entités OSM
      * @author Yura Tak (247528)
@@ -55,11 +58,11 @@ public abstract class OSMEntity {
      *
      */
     public static class Builder{
-        
+
         protected long id;
         protected Attributes.Builder attributes = new Attributes.Builder();
         private boolean isIncomplete = false;
-        
+
         /**
          * construit un bâtisseur pour une entité OSM identifiée par l'entier donné
          * @param id L'identifiant unique
@@ -67,7 +70,7 @@ public abstract class OSMEntity {
         public Builder(long id){
             this.id = id;
         }
-        
+
         /**
          * ajoute l'association (clef, valeur) donnée à l'ensemble d'attributs de l'entité en cours de construction. 
          * Si un attribut de même nom avait déjà été ajouté précédemment, sa valeur est remplacée par celle donnée
@@ -77,14 +80,14 @@ public abstract class OSMEntity {
         public void setAttribute(String key, String value){
             attributes.put(key, value);
         }
-        
+
         /**
          * déclare que l'entité en cours de construction est incomplète
          */
         public void setIncomplete(){
             isIncomplete = true;
         }
-        
+
         /**
          * retourne vrai si et seulement si l'entité en cours de construction est incomplète, 
          * c-à-d si la méthode setIncomplete a été appelée au moins une fois sur ce bâtisseur depuis sa création.
@@ -93,6 +96,6 @@ public abstract class OSMEntity {
         public boolean isIncomplete(){
             return isIncomplete;
         }
-        
+
     }
 }
--- src/ch/epfl/imhof/osm/OSMNode.java
+++ src/ch/epfl/imhof/osm/OSMNode.java
@@ -10,13 +10,8 @@ import ch.epfl.imhof.PointGeo;
  *
  */
 public final class OSMNode extends OSMEntity{
-    
-    private PointGeo position;
+    private final PointGeo position;
 
-    public OSMNode(long id, Attributes attributes) {
-        super(id, attributes);
-    }
-    
     /**
      * construit un nœud OSM avec l'identifiant, la position et les attributs donnés
      * @param id L'identifiant unique
@@ -25,9 +20,10 @@ public final class OSMNode extends OSMEntity{
      */
     public OSMNode(long id, PointGeo position, Attributes attributes){
         super(id, attributes);
+        if (position == null) throw new NullPointerException("Position cannot be null");
         this.position = position;
     }
-    
+
     /**
      * retourne la position du nœud
      * @return la position du noeud
@@ -35,7 +31,7 @@ public final class OSMNode extends OSMEntity{
     public PointGeo position(){
         return position;
     }
-    
+
     /**
      * sert de bâtisseur à la classe OSMNode et permet de construire un nœud en plusieurs étapes
      * @author Yura Tak (247528)
@@ -43,39 +39,34 @@ public final class OSMNode extends OSMEntity{
      *
      */
     public final static class Builder extends OSMEntity.Builder {
-        
-        private long id;
-        private PointGeo position;
-        private Attributes.Builder attributes = new Attributes.Builder();
-        private OSMNode node;
-        
+
+        private final long id;
+        private final PointGeo position;
+        private final Attributes.Builder attributes = new Attributes.Builder();
+
         /**
          * construit un bâtisseur pour un nœud ayant l'identifiant et la position donnés
          * @param id L'identifiant unique
          * @param position La position donnée
          */
         public Builder(long id, PointGeo position){
+            if (position == null) throw new NullPointerException("Position cannot be null");
             super(id);
             this.position = position;
         }
-        
+
         /**
-         * construit un nœud OSM avec l'identifiant et la position passés au constructeur, et les éventuels attributs ajoutés jusqu'ici au bâtisseur. 
-         * @throws IllegalStateException si le nœud en cours de construction est incomplet 
+         * construit un nœud OSM avec l'identifiant et la position passés au constructeur, et les éventuels attributs ajoutés jusqu'ici au bâtisseur.
+         * @throws IllegalStateException si le nœud en cours de construction est incomplet
          * (c-à-d si la méthode setIncomplete a été appelée sur ce bâtisseur depuis sa création)
-         * @return 
+         * @return
          */
         public OSMNode build(){
-            if(isIncomplete()){
+            if(isIncomplete())
                 throw new IllegalStateException("le noeud en cours de construction est incomplet");
-            } else{
-                node = new OSMNode(id, position, attributes.build());
-                return node;
-            }
+
+            return new OSMNode(id, position, attributes.build());
+
         }
-        
     }
-    
-    
-    
 }
