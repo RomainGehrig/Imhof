f41177991bdf327642b1e0e31524c5fcc917b6cf
Romain Gehrig <romain.gehrig@gmail.com>
2015-03-23 13:33:08 +0100

    OSMMapReaderTest

---
+++ test/ch/epfl/imhof/osm/OSMMapReaderTest.java
@@ -0,0 +1,18 @@
+package ch.epfl.imhof.osm;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.xml.sax.SAXException;
+import java.io.IOException;
+
+public class OSMMapReaderTest {
+
+    @Test
+    public void bcTest() throws SAXException,IOException {
+        String fileName = getClass().getResource("bc.osm").getFile();
+        OSMMap map = OSMMapReader.readOSMFile(fileName, false);
+    }
+}
