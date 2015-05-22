3dea6f1504d312f5c6eb902c4d6e01788d1fd022
Romain Gehrig <romain.gehrig@gmail.com>
2015-05-22 12:59:12 +0200

    Main done

--- src/ch/epfl/imhof/Main.java
+++ src/ch/epfl/imhof/Main.java
@@ -1,70 +1,110 @@
 package ch.epfl.imhof;
 
+import org.xml.sax.SAXException;
 import java.util.function.Predicate;
 import java.io.File;
 import java.io.IOException;
+import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
 import ch.epfl.imhof.PointGeo;
 import ch.epfl.imhof.geometry.Point;
-import ch.epfl.imhof.painting.*;
 import ch.epfl.imhof.osm.OSMMap;
 import ch.epfl.imhof.osm.OSMMapReader;
 import ch.epfl.imhof.osm.OSMToGeoTransformer;
-import ch.epfl.imhof.projection.*;
-
-
-// TESTS
+import ch.epfl.imhof.dem.Earth;
+import ch.epfl.imhof.dem.ReliefShader;
 import ch.epfl.imhof.dem.HGTDigitalElevationModel;
+import ch.epfl.imhof.painting.Color;
+import ch.epfl.imhof.painting.Java2DCanvas;
+import ch.epfl.imhof.painting.Painter;
+import ch.epfl.imhof.projection.Projection;
+import ch.epfl.imhof.projection.CH1903Projection;
 
+/**
+ * Point d'entrée principal du programme
+ * @author Yura Tak (247528)
+ * @author Romain Gehrig (223316)
+ *
+ */
 public class Main {
+
+    // Constantes
+    private static final Painter PAINTER = SwissPainter.painter();
+    private static final Projection PROJECTION = new CH1903Projection();
+    private static final Vector3 LIGHT_SOURCE = new Vector3(-1,1,1);
+
+    /**
+     * Point d'entrée principal du programme
+     * @param args Les arguments passés au programme
+     */
     public static void main(String[] args) {
-        Painter painter = SwissPainter.painter();
-        Projection p = new CH1903Projection();
-        OSMMap m = null;
-        Point bl = null;
-        Point tr = null;
-        Java2DCanvas canvas = null;
-        String filename = null;
+        OSMMap osmMap = null;
         HGTDigitalElevationModel dem = null;
-
         try {
-            dem = new HGTDigitalElevationModel(new File("data/imhof-dems/N46E007.hgt"));
+            osmMap = OSMMapReader.readOSMFile(args[0],true);
+            dem = new HGTDigitalElevationModel(new File(args[1]));
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+        catch (SAXException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        if (osmMap == null || dem == null) {
+            System.out.println("Something went wrong when reading files. Exiting");
+            System.exit(1);
+        }
+
+        PointGeo geoBL = new PointGeo(Math.toRadians(Double.parseDouble(args[2])),
+                                      Math.toRadians(Double.parseDouble(args[3])));
+        PointGeo geoTR = new PointGeo(Math.toRadians(Double.parseDouble(args[4])),
+                                      Math.toRadians(Double.parseDouble(args[5])));
+        Point bl = PROJECTION.project(geoBL);
+        Point tr = PROJECTION.project(geoTR);
+
+        int dpi = Integer.parseInt(args[6]);
+        File output = new File(args[7]);
+
+        double pixelsPerMeter = dpi * 39.370079; // valeur donnée par le programme `units`
+        int height = (int) Math.round(pixelsPerMeter * 1/25000d
+                                      * (geoTR.latitude() - geoBL.latitude())
+                                      * Earth.RADIUS);
+        int width = (int) Math.round((tr.x() - bl.x()) / (tr.y() - bl.y()) * height);
+
+        Map map = new OSMToGeoTransformer(PROJECTION).transform(osmMap);
+        Java2DCanvas canvas = new Java2DCanvas(bl, tr, width, height, dpi, Color.WHITE);
+
+        double gaussianBlurRadius = pixelsPerMeter * 1.7 * 1/1000d;
+        ReliefShader reliefShader = new ReliefShader(PROJECTION, dem, LIGHT_SOURCE);
+        BufferedImage reliefImg = reliefShader.shadedRelief(bl, tr, width, height, gaussianBlurRadius);
+
+        PAINTER.drawMap(map, canvas);
+
+        // Output
+        try {
+            ImageIO.write(mixImages(reliefImg, canvas.image()), "png", output);
         } catch (IOException e) {}
+    }
 
-        System.out.println("Bottom left X: " + dem.bottomLeftX(7.0));
-        System.out.println("Bottom left Y: " + dem.bottomLeftY(47.0));
+    private static BufferedImage mixImages(BufferedImage a, BufferedImage b) {
+        int width = a.getWidth();
+        int height = a.getHeight();
 
-        System.out.println(dem.normalAt(new PointGeo(Math.toRadians(7.0), Math.toRadians(47.0-0.0000000000001))).normalized());
+        if (width != b.getWidth() || height != b.getHeight())
+            throw new IllegalArgumentException("Images are not the same size");
 
-        try {
-            switch (0) {
-            case 0:
-                m = OSMMapReader.readOSMFile("data/lausanne.osm.gz",true); // Lue depuis lausanne.osm.gz
-                bl = new Point(532510, 150590);
-                tr = new Point(539570, 155260);
-                canvas = new Java2DCanvas(bl, tr, 1600, 1080, 150, Color.WHITE);
-                filename = "loz";
-                break;
-            case 1:
-                m = OSMMapReader.readOSMFile("data/interlaken.osm.gz",true);
-                bl = new Point(628590, 168210);
-                tr = new Point(635660, 172870);
-                canvas = new Java2DCanvas(bl, tr, 800, 530, 72, Color.WHITE);
-                filename = "inter";
-                break;
+        BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                Color c1 = Color.rgb(a.getRGB(x,y));
+                Color c2 = Color.rgb(b.getRGB(x,y));
+                result.setRGB(x,y, c1.mixWith(c2).toAWTColor().getRGB());
             }
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        Map map = (new OSMToGeoTransformer(p)).transform(m);
-
-        // Dessin de la carte et stockage dans un fichier
-        painter.drawMap(map, canvas);
-        try {
-            ImageIO.write(canvas.image(), "png", new File(filename + ".png"));
-        } catch (IOException e) {}
+        return result;
     }
 }
