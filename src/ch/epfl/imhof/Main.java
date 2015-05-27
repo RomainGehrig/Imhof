173d325e3615b0f931a43f2e65d440483ec85f3e
Romain Gehrig <romain.gehrig@gmail.com>
2015-05-27 16:10:58 +0200

    intermediate 3d model done

--- src/ch/epfl/imhof/Main.java
+++ src/ch/epfl/imhof/Main.java
@@ -21,13 +21,26 @@ import ch.epfl.imhof.painting.Painter;
 import ch.epfl.imhof.projection.Projection;
 import ch.epfl.imhof.projection.CH1903Projection;
 
+// TESTS
+import ch.epfl.imhof.dem.Mesh3D;
+import javafx.scene.paint.PhongMaterial;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.shape.MeshView;
+import javafx.scene.*;
+import javafx.scene.shape.CullFace;
+import javafx.collections.*;
+import javafx.scene.transform.Rotate;
+
+
 /**
  * Point d'entrée principal du programme
  * @author Yura Tak (247528)
  * @author Romain Gehrig (223316)
  *
  */
-public class Main {
+public class Main extends Application {
 
     // Constantes
     private static final Painter PAINTER = SwissPainter.painter();
@@ -39,11 +52,22 @@ public class Main {
      * @param args Les arguments passés au programme
      */
     public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        String[] args = getParameters().getRaw().toArray(new String[0]);
+
         OSMMap osmMap = null;
         HGTDigitalElevationModel dem = null;
+        /*
         try {
+            System.out.println("Reading OSM file");
             osmMap = OSMMapReader.readOSMFile(args[0],true);
+        */
             dem = new HGTDigitalElevationModel(new File(args[1]));
+            /*
         }
         catch (IOException e) {
             System.out.println(e.getMessage());
@@ -58,14 +82,14 @@ public class Main {
             System.out.println("Something went wrong when reading files. Exiting");
             System.exit(1);
         }
-
+        */
         PointGeo geoBL = new PointGeo(Math.toRadians(Double.parseDouble(args[2])),
                                       Math.toRadians(Double.parseDouble(args[3])));
         PointGeo geoTR = new PointGeo(Math.toRadians(Double.parseDouble(args[4])),
                                       Math.toRadians(Double.parseDouble(args[5])));
         Point bl = PROJECTION.project(geoBL);
         Point tr = PROJECTION.project(geoTR);
-
+        /*
         int dpi = Integer.parseInt(args[6]);
         File output = new File(args[7]);
 
@@ -75,21 +99,75 @@ public class Main {
                                       * Earth.RADIUS);
         int width = (int) Math.round((tr.x() - bl.x()) / (tr.y() - bl.y()) * height);
 
+        System.out.println("Transforming OSMMap");
         Map map = new OSMToGeoTransformer(PROJECTION).transform(osmMap);
         Java2DCanvas canvas = new Java2DCanvas(bl, tr, width, height, dpi, Color.WHITE);
 
+        System.out.println("Creating relief");
         double gaussianBlurRadius = pixelsPerMeter * 1.7 * 1/1000d;
         ReliefShader reliefShader = new ReliefShader(PROJECTION, dem, LIGHT_SOURCE);
         BufferedImage reliefImg = reliefShader.shadedRelief(bl, tr, width, height, gaussianBlurRadius);
 
+        System.out.println("Drawing in the canvas");
         PAINTER.drawMap(map, canvas);
 
         // Output
         try {
             ImageIO.write(mixImages(reliefImg, canvas.image()), "png", output);
         } catch (IOException e) {}
+
+        */
+        System.out.println("Getting the texture");
+        Image texture = new Image("file:" + args[7]);
+
+        System.out.println("Creating mesh");
+        Mesh3D mesh = new Mesh3D(geoBL, geoTR, 1000.0, dem);
+        mesh.construct();
+
+        MeshView meshView = new MeshView(mesh.mesh());
+
+        // Application de la texture
+        PhongMaterial material = new PhongMaterial();//javafx.scene.paint.Color.DARKGREEN);
+        material.setDiffuseMap(texture);
+
+        ObservableFloatArray points = mesh.mesh().getPoints();
+
+        showHeadTail(mesh.mesh().getPoints(), 10);
+
+        meshView.setMaterial(material);
+        meshView.setCullFace(CullFace.NONE);
+        meshView.setTranslateX(points.get(0));
+        meshView.setTranslateY(points.get(1));
+        meshView.setTranslateZ(points.get(2));
+        meshView.setRotationAxis(Rotate.X_AXIS);
+        meshView.setRotate(-60);
+        System.out.println("All done!");
+
+        //        for (int p = 0; p < 100; ++p) {
+        //            System.out.println("Point: " + points.get(3*p) + " x " + points.get(3*p + 1) + " y " + points.get(3*p + 2) + " z");
+        //        }
+
+        final Group root = new Group(meshView);
+        final Scene scene = new Scene(root, 1200, 1000, true);
+
+        scene.setCamera(new PerspectiveCamera(false));
+        stage.setScene(scene);
+        stage.show();
     }
 
+    private static void showHeadTail(ObservableFloatArray xs, int n) {
+        System.out.println("Array size: " + xs.size());
+        System.out.println("Head: ");
+        for (int x = 0; x < n; ++x) {
+            System.out.println(xs.get(x));
+        }
+        System.out.println();
+        System.out.println("Tail: ");
+        for (int x = xs.size() - n; x < xs.size(); ++x) {
+            System.out.println(xs.get(x));
+        }
+
+    }
     private static BufferedImage mixImages(BufferedImage a, BufferedImage b) {
         int width = a.getWidth();
         int height = a.getHeight();
---
+++ src/ch/epfl/imhof/dem/Mesh3D.java
@@ -0,0 +1,171 @@
+package ch.epfl.imhof.dem;
+
+import javafx.scene.shape.TriangleMesh;
+import java.util.function.Function;
+
+import ch.epfl.imhof.PointGeo;
+import ch.epfl.imhof.dem.Earth;
+import ch.epfl.imhof.dem.DigitalElevationModel;
+
+/**
+ * Représentation du mesh 3D basé sur les hauteurs
+ * données par un modèle digital d'élevation
+ * @author Romain Gehrig (223316)
+ *
+ */
+public final class Mesh3D {
+
+    private final TriangleMesh mesh;
+    private final DigitalElevationModel dem;
+    private final double cathetusLength;
+    private final PointGeo bl;
+    private final PointGeo tr;
+    private final double METERS_TO_RADIANS = 1d/Earth.RADIUS; // correspondance mètres -> radians
+
+    /**
+     * Création d'un mesh 3d
+     * @param bl Le point bas-gauche
+     * @param tr Le point haut-droit
+     * @param cathetusLength La longueur des cathètes de chaque triangle (en mètre)
+     * @param dem Le modèle digital d'élevation dont on tirera les données
+     */
+    public Mesh3D(PointGeo bl, PointGeo tr, double cathetusLength, DigitalElevationModel dem) {
+        mesh = new TriangleMesh();
+        this.dem = dem;
+        this.cathetusLength = cathetusLength;
+        this.bl = bl;
+        this.tr = tr;
+    }
+
+    public void construct() {
+
+        /*
+        float[] points_ = { 0,0,40,
+                            100,0,0,
+                            0,100,12,
+                            100,100,0 };
+
+        float[] textures_ = { 0,0,
+                              0,1,
+                              1,0,
+                              1,1 };
+
+        int[] triangles_ = { 0,0, 2,1, 1,2 };
+
+
+        mesh.getPoints().setAll(points_);
+        mesh.getFaces().setAll(triangles_);
+        mesh.getTexCoords().setAll(textures_);
+
+        if (true)
+            return;
+
+        */
+
+        // delta radians entre chaque PointGeo
+        double delta = cathetusLength * METERS_TO_RADIANS;
+
+        int nbrHorizontalPoints = (int) Math.floor((tr.longitude() - bl.longitude())/delta) + 1;
+        int nbrVerticalPoints = (int) Math.floor((tr.latitude() - bl.latitude())/delta) + 1;
+
+        Function<Double, Double> toXSize = (x) -> (x - bl.longitude()) / (tr.longitude() - bl.longitude())*500;
+        Function<Double, Double> toYSize = (y) -> 500 - (y - bl.latitude()) / (tr.latitude() - bl.latitude())*500;
+
+        // Il y a nbrHorizontalPoints * nbrVerticalPoints points, de chacun 3 coordonnées (x,y,z)
+        float[] points = new float[3 * nbrHorizontalPoints * nbrVerticalPoints];
+
+        System.out.println("Mesh: construction des points 3D");
+
+        float minHeight = (float) dem.interpolatedHeightAt(bl);
+        float maxHeight = minHeight;
+
+        int pos = 0; // position dans le tableau
+        double topLeftX = bl.longitude();
+        double topLeftY = tr.latitude();
+        System.out.println(topLeftX + " " + topLeftY);
+        for (int y = 0; y < nbrVerticalPoints; ++y) {
+            for (int x = 0; x < nbrHorizontalPoints; ++x) {
+                float height = (float) dem.interpolatedHeightAt(new PointGeo(topLeftX + x*delta,
+                                                                             topLeftY - y*delta));
+                minHeight = Math.min(minHeight, height);
+                maxHeight = Math.max(maxHeight, height);
+
+                points[pos + 0] = x / (float) nbrHorizontalPoints * 1000; //toXSize.apply(x).floatValue(); // composante x
+                points[pos + 1] = y / (float) nbrVerticalPoints * 1000; //toYSize.apply(y).floatValue(); // composante y
+                points[pos + 2] = height; // composante z
+                pos += 3;
+            }
+        }
+
+        float diffHeight = maxHeight - minHeight;
+        if (diffHeight != 0) {
+            for (int x = 2; x < points.length; x += 3) {
+                points[x] = 0;//Math.abs((points[x] - minHeight) / diffHeight) * -200;
+            }
+        }
+
+        mesh.getPoints().setAll(points);
+
+        float[] textures = new float[2 * nbrHorizontalPoints * nbrVerticalPoints];
+
+        System.out.println("Mesh: construction des textures");
+        pos = 0;
+        for (int y = 0; y < nbrVerticalPoints; ++y) {
+            for (int x = 0; x < nbrHorizontalPoints; ++x) {
+                textures[pos + 0] = (float) x / (float) (nbrHorizontalPoints - 1);
+                textures[pos + 1] = (float) y / (float) (nbrVerticalPoints - 1);
+                pos += 2;
+            }
+        }
+
+        mesh.getTexCoords().setAll(textures);
+
+        System.out.println("Mesh: construction des triangles");
+        // Il y a 2 triangles de 3+3 points pour chaque carré de 4 points
+        // il y a (nbrHorizontalPoints - 1) * (nbrVerticalPoints - 1) carrés
+        int[] triangles = new int[2 * 6 * (nbrHorizontalPoints - 1) * (nbrVerticalPoints - 1)];
+
+        // Construction des triangles:
+        //
+        //   Les points doivent être indiqués dans le sens antihoraire pour
+        // construire des triangles dont la face visible est dirigée vers
+        // le haut.
+        //   On construit les triangles deux par deux en utilisant les 4
+        // points d'un carré:
+        //      ___
+        //   |\ \ | 2
+        // 1 |_\ \|
+        //
+        pos = 0;
+        for (int vs = 0; vs < nbrVerticalPoints - 1; ++vs) {
+            for (int hs = 0; hs < nbrHorizontalPoints - 1; ++hs) {
+                int pos2 = hs + (nbrHorizontalPoints-1)*vs;
+
+                // Triangle bas-gauche (1)
+                triangles[12*pos + 0] = pos2;
+                triangles[12*pos + 1] = pos2;
+                triangles[12*pos + 2] = pos2 + nbrHorizontalPoints;
+                triangles[12*pos + 3] = pos2 + nbrHorizontalPoints;
+                triangles[12*pos + 4] = pos2 + nbrHorizontalPoints + 1;
+                triangles[12*pos + 5] = pos2 + nbrHorizontalPoints + 1;
+
+                // Triangle haut-droite (2)
+                triangles[12*pos + 6] = pos2;
+                triangles[12*pos + 7] = pos2;
+                triangles[12*pos + 8] = pos2 + nbrHorizontalPoints + 1;
+                triangles[12*pos + 9] = pos2 + nbrHorizontalPoints + 1;
+                triangles[12*pos + 10] = pos2 + 1;
+                triangles[12*pos + 11] = pos2 + 1;
+
+                pos += 1;
+            }
+        }
+
+        mesh.getFaces().setAll(triangles);
+
+    }
+
+    public TriangleMesh mesh() {
+        return mesh;
+    }
+}
